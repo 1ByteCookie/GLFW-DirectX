5b7415e3eac86f9b11530e6040ce700a31752c98
1ByteCookie <melancholic.children@gmail.com>
2022-10-14 14:36:55 +0800

    build script to put dependencies in output directory

---
+++ build.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import sys
+
+# This script will be invoked by Visual Studio's post build events which can be found in the project settings
+# The command line arguments will be passed by Visual Studio macros
+
+
+SolutionDir = sys.argv[1]
+OutputDir = sys.argv[2]
+GlfwDependency = "Dependencies/GLFW/lib/glfw3.dll"
+
+
+os.chdir("../")
+print("PYTHON CWD: " + os.getcwd());
+
+
+shutil.copy(GlfwDependency, OutputDir)
+
+
+print("PYTHON BUILD SCRIPT INVOKED")
